f50347af860da4545ad4646008e2ca20c324d291
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-07-24 16:36:20 -0300

    Calendário e disponibilidade - Estilização do calendário

--- src/app/_components/Calendar/styles.ts
+++ src/app/_components/Calendar/styles.ts
@@ -3,14 +3,97 @@
 import { Text } from '@beryl-ui/react'
 import styled from 'styled-components'
 
-export const CalendarContainer = styled.div``
+export const CalendarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.space[6]};
+  padding: ${({ theme }) => theme.space[6]};
+`
 
-export const CalendarHeader = styled.div``
+export const CalendarHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
-export const CalendarTitle = styled(Text)``
+export const CalendarTitle = styled(Text)`
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
 
-export const CalendarActions = styled.div``
+  & span {
+    color: ${({ theme }) => theme.colors.gray200};
+  }
+`
 
-export const CalendarBody = styled.table``
+export const CalendarActions = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.space[2]};
+  color: ${({ theme }) => theme.colors.gray200};
 
-export const CalendarDay = styled.button``
+  & button {
+    all: unset;
+    cursor: pointer;
+    line-height: 0;
+    border-radius: ${({ theme }) => theme.radii.sm};
+
+    svg: {
+      width: ${({ theme }) => theme.space[5]};
+      height: ${({ theme }) => theme.space[5]};
+    }
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.gray100};
+    }
+
+    &:focus {
+      box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.gray100};
+    }
+  }
+`
+
+export const CalendarBody = styled.table`
+  width: 100%;
+  font-family: ${({ theme }) => theme.fonts.default};
+  border-spacing: 0.25rem;
+  table-layout: fixed;
+
+  & thead th {
+    color: ${({ theme }) => theme.colors.gray200};
+    font-weight: ${({ theme }) => theme.fontWeights.medium};
+    font-size: ${({ theme }) => theme.fontSizes.sm};
+  }
+
+  & tbody:before {
+    content: '.';
+    display: block;
+    line-height: 0.75rem;
+    color: ${({ theme }) => theme.colors.gray800};
+  }
+
+  & tbody td {
+    box-sizing: border-box;
+  }
+`
+
+export const CalendarDay = styled.button`
+  all: unset;
+  width: 100%;
+  aspect-ratio: 1/1;
+  background: ${({ theme }) => theme.colors.gray600};
+  text-align: center;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.radii.sm};
+
+  &:disabled {
+    background: none;
+    cursor: default;
+    opacity: 0.4;
+  }
+
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme.colors.gray500};
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.gray100};
+  }
+`
--- src/app/schedule/_components/ScheduleForm/CalendarStep/styles.ts
+++ src/app/schedule/_components/ScheduleForm/CalendarStep/styles.ts
@@ -6,8 +6,9 @@ import { styled } from 'styled-components'
 export const Container = styled(Box)`
   position: relative;
   display: grid;
+  grid-template-columns: 1fr;
+  width: 540px;
   max-width: 100%;
-
   margin: ${({ theme }) => theme.space[6]} auto 0;
   padding: 0;
 `
