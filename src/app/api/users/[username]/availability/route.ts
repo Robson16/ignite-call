3b0a1a325b0374277c44f65ef6546dddf06f3290
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-07-28 14:19:53 -0300

    Calendário e disponibilidade - Rota de horários disponíveis

---
+++ src/app/api/users/[username]/availability/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getServerSession } from 'next-auth'
+
+import dayjs from '@/app/_lib/dayjs'
+import { prisma } from '@/app/_lib/prisma'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { username: string } },
+) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+  }
+
+  const { username } = await params
+  const date = request.nextUrl.searchParams.get('date')
+
+  if (!date) {
+    return NextResponse.json({ message: 'Date not provided.' }, { status: 400 })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return NextResponse.json(
+      { message: 'User does not exist.' },
+      { status: 400 },
+    )
+  }
+
+  const referenceDate = dayjs(date)
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return NextResponse.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return NextResponse.json({ availability: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+
+  const startHour = Math.ceil(time_start_in_minutes / 60)
+  const endHour = Math.floor(time_end_in_minutes / 60)
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, index) => {
+      return startHour + index
+    },
+  )
+
+  return NextResponse.json({ possibleTimes })
+}
