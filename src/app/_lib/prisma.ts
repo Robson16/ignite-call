f493a9296a8d3546d8fd5f5e069f8178ecaac408
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-08-03 20:18:01 -0300

    Deploy do projeto - Alterando banco para PostgreSQL

--- src/app/_lib/prisma.ts
+++ src/app/_lib/prisma.ts
@@ -1,5 +1,24 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  log: ['query'],
-})
+const connectionString = process.env.DATABASE_URL!
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: connectionString,
+      },
+    },
+    log: ['query'],
+  })
+}
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma
+}
